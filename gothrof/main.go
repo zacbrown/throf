27702f3dd17d04db06e52edf1d536be8d5925922
Zac Brown <zac@nworbcaz.com>
2015-02-09 07:50:22 -0800

    Initial commit of gothrof code.
    Some of this is based off of the goforth code since there are
    similarities in parsing and tokenizing.

---
+++ gothrof/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		usage()
+		return
+	}
+
+	file := os.Args[1]
+	dat, _ := getFileAsString(file)
+
+	tokens := tokenize(dat)
+}
+
+func usage() {
+	fmt.Println("Usage: ./gothrof <code file>.th4")
+}
+
+func getFileAsString(file string) (string, error) {
+	data, err := ioutil.ReadFile(file)
+	return string(data), err
+}
+
+func tokenize(input string) list.List {
+	reader := strings.NewReader(input)
+	var buffer bytes.Buffer
+	var tokens list.List
+
+	for ch, _, err := reader.ReadRune(); err == nil; ch, _, err = reader.ReadRune() {
+		if ch == '\t' || ch == ' ' || ch == '\n' || ch == '\r' || ch == '\f' {
+			if buffer.Len() > 0 {
+				tokens.PushBack(buffer.String())
+				buffer.Reset()
+			}
+		} else {
+			buffer.WriteRune(ch)
+		}
+	}
+
+	return tokens
+}

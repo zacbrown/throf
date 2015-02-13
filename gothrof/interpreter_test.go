7da4e286b929bd1a0c783fc3450db23d558fbedb
Zac Brown <zac@nworbcaz.com>
2015-02-13 08:58:38 -0800

    Add test for dup.

--- gothrof/interpreter_test.go
+++ gothrof/interpreter_test.go
@@ -50,3 +50,36 @@ func TestSwap(t *testing.T) {
 		t.Errorf("Expected stack depth of 0, got %d", interpreter.GetDStack().Length())
 	}
 }
+
+func TestDup(t *testing.T) {
+	t.Log("Testing 'dup'")
+
+	toks := tokenize("2 dup")
+	interpreter.Init(toks)
+
+	// step one to make sure 2 is pushed
+	interpreter.Step()
+	dstack := interpreter.GetDStack()
+
+	top := dstack.Peek()
+	if top != "2" {
+		t.Errorf("Expected '2' to be on top of dstack, got %d", top)
+	}
+
+	interpreter.Execute()
+
+	if dstack.Length() != 2 {
+		t.Errorf("Expected two elements on the dstack, got depth %d", dstack.Length())
+	}
+
+	newTop := dstack.Pop()
+	next := dstack.Pop()
+
+	if top != next {
+		t.Errorf("Expected the two elements on the dstack to be equal. top: %d, next: %d", top, next)
+	}
+
+	if newTop != top {
+		t.Errorf("Expected two elements of same value on dstack. expected: %d, got: %d", top, newTop)
+	}
+}

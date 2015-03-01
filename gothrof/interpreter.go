e477a1c4f8ec42fce359b38b3862a61d7c7527d2
Zac Brown <zac@nworbcaz.com>
2015-03-01 12:22:05 -0800

    Nuke the opcode constants. We don't actually need them.

--- gothrof/interpreter.go
+++ gothrof/interpreter.go
@@ -9,52 +9,8 @@ import (
 	"strings"
 )
 
-const (
-	OP_DROP      = iota
-	OP_SWAP      = iota
-	OP_DUP       = iota
-	OP_OVER      = iota
-	OP_ROT       = iota
-	OP_NROT      = iota
-	OP_2DROP     = iota
-	OP_2DUP      = iota
-	OP_QDUP      = iota // ?dup
-	OP_INCR      = iota
-	OP_DECR      = iota
-	OP_ADD       = iota
-	OP_SUB       = iota
-	OP_MUL       = iota
-	OP_DIVMOD    = iota
-	OP_EQU       = iota
-	OP_NEQU      = iota
-	OP_LT        = iota
-	OP_GT        = iota
-	OP_LE        = iota
-	OP_GE        = iota
-	OP_ZEQU      = iota
-	OP_ZNEQU     = iota
-	OP_ZLT       = iota
-	OP_ZGT       = iota
-	OP_ZLE       = iota
-	OP_ZGE       = iota
-	OP_AND       = iota
-	OP_OR        = iota
-	OP_XOR       = iota
-	OP_CREATE    = iota
-	OP_COMMA     = iota
-	OP_LBRAC     = iota // switch to immediate mode, immediate word
-	OP_RBRAC     = iota // switch to compile mode
-	OP_COLON     = iota
-	OP_SEMICOLON = iota // immediate word
-	OP_BRANCH    = iota
-	OP_ZBRANCH   = iota
-	OP_IMMEDIATE = iota
-)
-
 type CodeWord func(*Interpreter)
-
 type StringLiteral string
-
 type Quotation *list.List
 
 type Word struct {
